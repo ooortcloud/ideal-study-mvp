31370c610e6789851bad63e5b3bb854c814218e9
SEONG JIN JO <123255935+seong-jin-jo@users.noreply.github.com>
2024-11-29 14:56:52 +0900

    fix : 프론트 토큰 요청 관련 소소한 업데이트

--- frontend/src/services/AuthService.mjs
+++ frontend/src/services/AuthService.mjs
@@ -10,7 +10,7 @@ export const loginUser = async (username, password) => {
         username,
         password,
       });
-    
+      console.log("로그인 API 성공:", response)
       // 서버에서 받은 JWT 토큰과 유저 정보 반환
       const { token, user } = response.data;  // 서버에서 반환된 데이터 구조에 맞게 수정 필요
       return { token, user };  // 토큰과 유저 정보를 객체로 반환
