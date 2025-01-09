01c7c397366fc304f9f1e817ba65478b38fabd5d
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2025-01-09 19:06:24 +0900

    update: 유저 목록 출력 시 데이터 추가
    Role 데이터를 추가하였음. front에서 쉽게 작업할 수 있도록 돕는 의도로 수정함.

--- src/main/java/com/idealstudy/mvp/application/dto/member/MemberListDto.java
+++ src/main/java/com/idealstudy/mvp/application/dto/member/MemberListDto.java
@@ -1,5 +1,6 @@
 package com.idealstudy.mvp.application.dto.member;
 
+import com.idealstudy.mvp.enums.member.Role;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,6 +13,9 @@ public class MemberListDto {
     private String userId;
 
     private String name;
+
+
+    private Role role;
 
     // 나중에 uri로 변경해야 함
     private byte[] profile;
