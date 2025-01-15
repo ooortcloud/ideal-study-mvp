8664658df64c643a4b0a5a9f3318a9827a7053a8
SEONG JIN JO <123255935+seong-jin-jo@users.noreply.github.com>
2025-01-16 01:35:45 +0900

    feat : 최초로그인검사 후 페이지라우팅 구현

--- frontend/src/pages/auth/LoginPage.js
+++ frontend/src/pages/auth/LoginPage.js
@@ -12,19 +12,20 @@ const LoginPage = () => {
 
   const handleLogin = async () => {
     try {
-      const { token, user } = await loginUser(userEmail, password);
+      const { response, firstLoginResponse } = await loginUser(
+        userEmail,
+        password
+      );
 
-      login({ token, user }); // context 를 로그인상태로 등록
-      navigate("/"); // 로그인시 메인 페이지로 이동
+      const userId = login(response.data.username); // context 를 로그인상태로 등록
+
+      if (firstLoginResponse.data === "first") {
+        navigate(`/myPage/${userId}`);
+      } else {
+        navigate("/"); // 로그인시 메인 페이지로 이동
+      } // 로그인시 메인 페이지로 이동
     } catch (error) {
       console.log(error);
-
-      //////////////임시로 로그인처리 아래 함수들 삭제해야함
-      navigate("/");
-      login({
-        token: "dummy-token", // 임시로 더미 토큰
-        user: { name: "김동은", id: "2", level: "7", role: "teacher" },
-      });
     }
   };
 
